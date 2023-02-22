6128ecf977f08c07e8d7cdcb9ee7f4d502a26a32
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-02-22 15:49:10 +0100

    print output

--- script.py
+++ script.py
@@ -2,5 +2,9 @@ from ultralytics import YOLO
 
 
 model = YOLO("yolov8n.pt")
-model.predict(source='https://media.roboflow.com/notebooks/examples/dog.jpeg', conf=0.25)
+output = model.predict(
+        source='https://media.roboflow.com/notebooks/examples/dog.jpeg',
+        conf=0.25
+)
+print(output)
 
